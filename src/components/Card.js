8efe0f93eae7b73b61db5bfea80b7dac3e967ef4
Farai Major <faraimutukumira@gmail.com>
2022-01-12 19:07:01 -0500

    closes #3, styled the page

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,8 +8,11 @@ export default function Card(props){
     return (         
         <div className="card">
         <div className="card-stats">  
-            <span id="city-state">{zip.City}, {zip.State}</span>           
+        <div className="city-el">
+            <span id="city-state">{zip.City}, {zip.State}</span>  
+          </div>         
             <ul id="zip-detail">
+            <li>State: {zip.State}</li>
                 <li>Location: ({zip.Xaxis}, {zip.Yaxis}) </li>
                 <li>Population (estimated): {zip.EstimatedPopulation}</li>
                 <li>Total Wages: $ {zip.TotalWages}</li>
--- src/components/Zip2.js
+++ src/components/Zip2.js
@@ -1,55 +1,53 @@
-
 import React from 'react'
 import Card from './Card'
 
 export default function Zip() {
-    const[zipData, setZipData] = React.useState("")
+    const [zipData, setZipData] = React.useState("")
     const [userInput, setUserInput] = React.useState('')
- 
+
     // for dependancy value count : maybe can be changed after handleChange 
     const [count, setCount] = React.useState(0);
-  
-  
+
+
     React.useEffect(function() {
-        console.log("Effect ran")
-        console.log(count)
-        console.log("userInput:" +userInput)
-       
-        //setZipData updates state of zipData object
-        // Card is the child 
-        fetch(`http://ctp-zip-api.herokuapp.com/zip/${userInput}`)
-            .then(res => res.json())
-            .then(data => setZipData(data.map((elm) => 
-                <Card 
-                    key={elm.RecordNumber}
-                    type={elm}
-                />))
-            )                           
-    }, [count]);
+                console.log("Effect ran")
+                console.log(count)
+                console.log("userInput:" + userInput)
 
-    return (
-      
-        <div class="body">
-            <h1>Zip Code Search</h1>        
-            <form id="input-form">
-                <input
-                     type="text"
-                     placeholder="Zip Code"
-                     onChange={e => setUserInput(e.target.value)}
-                     name="userInput"
-                     value={userInput}
-                />            
-            </form>            
-          
-            <button onClick={() => setCount(prevCount => prevCount + 1)} id='submit-button'>Submit</button>
-            
-            <div id = "print-area">
-                <p id='list'>Result: </p>                 
-                {zipData}              
-            </div>        
-        </div>
-    ) 
+                //setZipData updates state of zipData object
+                // Card is the child 
+                fetch(`http://ctp-zip-api.herokuapp.com/zip/${userInput}`)
+                    .then(res => res.json())
+                    .then(data => setZipData(data.map((elm) =>
+                            <
+                            Card key = { elm.RecordNumber }
+                            type = { elm }
+                            />))
+                        )
+                    }, [count]);
 
-}
+                return (
 
+                    <div class = "body" >
+                    <div class = "zipbar">
+                    <label>Enter Zip Code:</label>
+                    <input type = "text"
+                    placeholder = "Zip Code..."
+                    onChange = { e => setUserInput(e.target.value) }
+                    name = "userInput"
+                    value = { userInput }
+                    />  
+         
+                    <button onClick = {
+                        () => setCount(prevCount => prevCount + 1) }
+                    id = 'submit-button' > Submit < /button>
 
+                    <div id = "print-area" >
+                    {/*<p id = 'list' > Result: < /p>    */}
+                  { zipData } 
+                    </div>        
+                     </div>
+                     </div>         
+                )
+
+            }
